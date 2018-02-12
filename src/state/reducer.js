4dd24cfb93f626116b02cdd29317552d6f577521
KKUSIAKU <kusiaku.k@gmail.com>
2018-02-12 14:53:54 +0000

    initial server rendering & static datas

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -1,19 +1,9 @@
-import config from "../cconfig.js"; 
 
 function rootReducer(state={}, action){
 
   if(action.type =="GET-MOVIES"){
     console.log("action catch up"); 
-    const MongoClient = require("mongodb").MongoClient; 
-    MongoClient.connect(config.uri, {nativeparser:true}, (err, database) =>{
-      if (err) {
-        console.log("connection failed"); 
-        return state; 
-      }
-      console.log("connection ok");
-      return database; 
-      
-    });
+
     return state; 
   }
   return state; 
